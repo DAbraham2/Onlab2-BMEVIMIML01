f6ae8773557bfba287fbbfbe5981efa4521fc5a3
DAbraham2 <daniel.abraham@edu.bme.hu>
2022-10-04 19:48:45 +0200

    main.cpp finalized

--- src/Demo1/main.cpp
+++ src/Demo1/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string str;
+    std::cout << "enter input string";
+
+    std::getline(std::cin, str);
+
+    std::cout << str << std::endl << str[0] << std::endl;
+
+    if (str[0] == 0 || str[str.length() - 1] == 0) {
+        abort();
+    } else {
+        int count = 0;
+        char prev_num = 'x';
+        while (count != str.length() - 1) {
+            char c = str[count];
+            if (c >= 48 && c <= 57) {
+                if (c == prev_num + 1) {
+                    abort();
+                }
+                prev_num = c;
+            }
+            count++;
+        }
+    }
     return 0;
 }
